695e0a88c26af3123807e6305bd188a6dd7d20a1
anbarasant14 <kishoredass5695@gmail.com>
2024-02-10 16:53:28 +0530

    modifiedfirstprogram

--- myfirstprogram.js
+++ myfirstprogram.js
@@ -1,7 +1,7 @@
 var product="macbook"
 var price=200000
-var discount=20000
+var discount=20000;
 
 console.log(product)
 var finalPrice=price-discount
-console.log(finalPrice)
+console.log(finalPrice);
